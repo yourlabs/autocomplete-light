8dd7fa7ab3c13dfbd5c7131c607ce625900455ce
jpic <jpic@yourlabs.org>
2020-12-03 17:30:48 +0100

    New version WIP

---
+++ new/autocomplete-light.js
@@ -0,0 +1,399 @@
+class AutocompleteLight extends HTMLElement {
+  box = null
+  xhr = null
+  timeoutId = null
+  input = null
+  box = null
+  options = {
+    url: null,
+    minimumCharacters: 0,
+    choiceSelector: '[data-value]'
+  }
+
+  constructor() {
+    super()
+  }
+
+  onInput() {
+    // clear any unset xhr
+    this.xhr && this.xhr.readyState === 0 && this.xhr.abort()
+    // clear any planned xhr
+    this.timeoutId && window.clearTimeout(this.timeoutId)
+    // plan an xhr
+    this.timeoutId = window.setTimeout(this.download.bind(this), 200)
+  }
+
+  hilight(choice) {
+    this.selected.forEach((item) => item.classList.remove('hilight'))
+    choice.classList.add('hilight')
+  }
+
+  selectChoice(choice) {
+    let eventName = 'autocompleteChoiceSelected'
+    let data = {choice}
+    if (window.CustomEvent && typeof window.CustomEvent === 'function') {
+      var event = new CustomEvent(eventName, {detail: data});
+      this.dispatchEvent(event)
+    } else {
+      var event2 = document.createEvent('CustomEvent');
+      event2.initCustomEvent(eventName, true, true, data);
+      this.dispatchEvent(event2)
+    }
+    this.box.setAttribute('hidden', 'true')
+  }
+
+  download() {
+    if (this.options.url) {
+      this.xhr = new XMLHttpRequest()
+      this.xhr.addEventListener('load', this.receive.bind(this))
+      this.xhr.open('GET', this.options.url)
+      this.xhr.send()
+    } else if (this.parentNode['tagName'] == 'AUTOCOMPLETE-SELECT') {
+      this.receive({
+        target: {
+          response: Array.from(
+              this.parentNode.querySelectorAll('option')
+          ).map(
+            (item) => `<div data-value="${item.getAttribute('value')}">${item.innerHTML}</div>`
+          ).join('\n'),
+        }
+      })
+    } else {
+      // test mode
+      this.receive({
+        target: {
+          response: ['aaa', 'aab', 'abb', 'bbb'].filter(
+            (item) => item.startsWith(this.input.value)
+          ).map(
+              (item) => `<div data-value="${item}">${item}</div>`
+          ).join('\n'),
+        }
+      })
+    }
+  }
+
+
+  keyboard(ev) {
+    switch(ev.keyCode) {
+      case 40: // down arrow
+      case 38: // up arrow
+      case 16: // shift
+      case 17: // ctrl
+      case 18: // alt
+        this.move(ev);
+        break;
+
+      case 13: // enter
+        if (this.box.getAttribute('hidden')) return
+
+        var choice = this.box.querySelector('.hilight');
+
+        if (!choice) {
+            // Don't get in the way, let the browser submit form or focus
+            // on next element.
+            return
+        }
+
+        ev.preventDefault()
+        ev.stopPropagation()
+
+        this.selectChoice(choice)
+        break
+
+      case 27: // escape
+        this.box.setAttribute('hidden', 'true')
+        break
+    }
+  }
+
+  move(ev) {
+    // If the autocomplete should not be displayed then return.
+    if (this.input.value.length < this.options.minimumCharacters) return true;
+
+    // Avoid moving the cursor in the input.
+    ev.preventDefault()
+    ev.stopPropagation()
+
+    // The current choice if any.
+    var current = this.box.querySelector('.hilight');
+
+    // If not KEY_UP or KEY_DOWN, then return.
+    // NOTE: with Webkit, both keyCode and charCode are set to 38/40 for &/(.
+    //       charCode is 0 for arrow keys.
+    //       Ref: http://stackoverflow.com/a/12046935/15690
+    var way;
+    if (ev.keyCode === 38 && !ev.charCode) way = 'up';
+    else if (ev.keyCode === 40 && !ev.charCode) way = 'down';
+    else return;
+
+    // The first and last choices. If the user presses down on the last
+    // choice, then the first one will be hilighted.
+    var first = this.choices[0];
+    var last = this.choices[this.choices.length - 1];
+
+    // The choice that should be hilighted after the move.
+    var target;
+
+    // The autocomplete must be shown so that the user sees what choice
+    // he is hilighting.
+    this.draw()
+
+    // If a choice is currently hilighted:
+    if (current) {
+      if (way === 'up') {
+        var next = this.choices.indexOf(current) - 1
+        target = next < 0 ? last : this.choices[next]
+      } else {
+        var next = this.choices.indexOf(current) + 1
+        target = next >= this.choices.length ? first : this.choices[next]
+      }
+    } else {
+      target = way === 'up' ? last : first;
+    }
+
+    this.hilight(target)
+  }
+
+  get choices() {
+    return Array.from(this.box.querySelectorAll(this.options.choiceSelector))
+  }
+
+  get selected() {
+    return this.box.querySelectorAll(this.options.choiceSelector + '.hilight')
+  }
+
+  receive(ev) {
+    this.draw()
+    this.box.innerHTML = ev.target.response
+    this.box.querySelectorAll(this.options.choiceSelector).forEach((item) => {
+      // item idempotence
+      if (item.getAttribute('data-bound'))
+        return
+
+      // bind mouse events
+      item.addEventListener(
+        'mouseenter',
+        (ev) => this.hilight(ev.target)
+      )
+      item.addEventListener(
+        'mouseleave',
+        (ev) => ev.target.classList.remove('hilight')
+      )
+      item.addEventListener(
+        'mousedown',
+        (ev) => this.selectChoice(ev.target)
+      )
+
+      // idempotence mark
+      item.setAttribute('data-bound', 'true')
+    })
+  }
+
+  boxBuild() {
+    this.box = document.createElement('div')
+    this.box.classList.add('autocomplete-light-box')
+    document.querySelector('body').appendChild(this.box)
+
+
+    this.input.addEventListener(
+      'focusout',
+      () => this.box.setAttribute('hidden', 'true')
+    )
+    this.input.addEventListener(
+      'blur',
+      () => this.box.setAttribute('hidden', 'true')
+    )
+  }
+
+  draw() {
+    if (!this.box) this.boxBuild()
+    var rect = this.input.getBoundingClientRect()
+    this.box.style.top = rect.bottom + 'px'
+    this.box.style.left = rect.left + 'px'
+    // keep some space for the border, avoid overflow on x
+    this.box.style.width = rect.width - 2 + 'px'
+    this.box.removeAttribute('hidden')
+  }
+
+  connectedCallback() {
+    this.input = this.querySelector('[slot=input]')
+    console.log(this)
+    console.log(this.querySelector('[slot=input]'))
+    if (this.hidden) {
+      this.input.setAttribute('hidden', 'true')
+    } else {
+      this.input.removeAttribute('hidden')
+    }
+
+    if (!this.input.getAttribute('data-bound')) {
+      this.input.addEventListener(
+        'focus',
+        () => this.input.value.length >= this.options.minimumCharacters && this.onInput()
+      )
+      this.input.addEventListener('keyup', this.keyboard.bind(this))
+      this.input.addEventListener('input', this.onInput.bind(this))
+      this.input.setAttribute('data-bound', 'true')
+      window.addEventListener(
+        'resize',
+        () => this.box && this.box.setAttribute('hidden', 'true')
+      )
+    }
+    console.log('connected')
+  }
+
+  disconnectedCallback() {
+    console.log('disconnected')
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    console.log('changed')
+  }
+}
+
+class AutocompleteSelect {
+  maxChoices = 0
+  multiple = false
+  url = null
+  bound = false
+  el = null
+  name = null
+
+  connectedCallback() {
+    if (!this.multiple) {
+      this.maxChoices = 1
+    }
+
+    // ensure all selected options are in deck
+    Array.from(
+      this.select.querySelectorAll('option[selected]')
+    ).map((option) => {
+      var cmp = document.createElement('div')
+      cmp.setAttribute('selected', 'selected')
+      cmp.setAttribute('data-value', option.getAttribute('value'))
+      cmp.innerHTML = option['innerHTML']
+      this.choiceSelect(cmp, false)
+    })
+
+    // ensure all deck values are in select
+    Array.from(
+      this.deck.querySelectorAll('[data-value]')
+    ).map((choice) => {
+        if (!this.select.querySelector('option[value="' + choice.getAttribute('data-value') + '"]')) {
+          this.choiceSelect(choice, false)
+        }
+        this.addClear(choice)
+    })
+  }
+
+  get deck() {
+    return this.el.querySelector('[slot=deck]')
+  }
+
+  get select() {
+    return this.el.querySelector('[slot=select]')
+  }
+
+  get selected() {
+    return this.deck.querySelectorAll('[data-value]')
+  }
+
+  get input() {
+    return this.el.querySelector('autocomplete-light')
+  }
+
+  onClearClick(ev) {
+    this.choiceUnselect(ev.target.parentNode)
+  }
+
+  choiceUnselect(choice, noShowHide = false) {
+    var value = choice.getAttribute('data-value')
+
+    var option = this.select.querySelector('option[value="' + value + '"]')
+    if (option) {
+      option.removeAttribute('selected')
+    }
+
+    var decked = this.deck.querySelector('[data-value="' + value + '"]')
+    if (decked) {
+      decked.parentNode.removeChild(decked)
+    }
+
+    if (!noShowHide)
+      this.input.hidden = this.maxChoices && this.selected.length >= this.maxChoices
+
+    this.changeTrigger()
+  }
+
+  choiceSelect(choice, trigger=true, option=false) {
+    console.log('choiceSelect', choice)
+
+    if (this.maxChoices && this.selected.length >= this.maxChoices) {
+      this.choiceUnselect(this.selected[0], true)
+    }
+
+    var value = choice.getAttribute('data-value')
+
+    // insert option in select if not present
+    option = this.select.querySelector('option[value="' + value + '"]')
+    if (!option) {
+      option = document.createElement('option')
+      option.setAttribute('value', value)
+      option.innerHTML = choice.innerHTML
+      this.select.appendChild(option)
+      console.log('select new option', option)
+    }
+    option.setAttribute('selected', 'selected')
+
+    // insert choice on deck if not present, based on a choice node clone
+    if (! this.deck.querySelector('[data-value="' + value + '"]')) {
+      choice = choice.cloneNode(9)
+      choice.classList.remove('hilight')
+      this.addClear(choice)
+      this.deck.appendChild(choice)
+      console.log('deck new choice', choice)
+    }
+
+    this.input.hidden = this.maxChoices && this.selected.length >= this.maxChoices
+
+    trigger && this.changeTrigger()
+  }
+
+  changeTrigger() {
+    this.select.dispatchEvent(
+      new Event('change', {bubbles: true, cancelable: false})
+    )
+  }
+
+  addClear(choice) {
+    if (choice.querySelector('.clear'))
+      return
+    var clear = document.createElement('span')
+    clear.classList.add('clear')
+    clear.addEventListener('click', this.onClearClick.bind(this))
+    clear.innerHTML = '✖'
+    choice.appendChild(clear)
+  }
+
+  componentDidRender() {
+    if (!this.input.getAttribute('data-bound')) {
+      this.input.addEventListener(
+        'autocompleteChoiceSelected',
+        (ev) => this.choiceSelect(ev.detail.choice)
+      )
+      this.input.setAttribute('data-bound', 'true')
+    }
+    this.input.hidden = this.maxChoices && this.selected.length >= this.maxChoices
+  }
+
+  /*
+render() {
+    return <Host class="autocomplete-select">
+      <slot name="select" />
+      <slot name="input" />
+      <slot name="deck" />
+    </Host>;
+  } */
+}
+
+window.customElements.define('autocomplete-light', AutocompleteLight);
+window.customElements.define('autocomplete-select', AutocompleteSelect);
---
+++ new/test.py
@@ -0,0 +1,124 @@
+from selenium.webdriver.common.keys import Keys
+
+
+def retry(cb, *args, expected=None, **kwargs):
+    i = 15
+    while True:
+        try:
+            result = cb(*args, **kwargs)
+        except:
+            if not i:
+                raise
+        else:
+            if expected is not None and result == expected:
+                return result
+            elif expected is None and result:
+                return result
+        i -= 1
+    return wrapper
+
+
+class AutocompleteLight:
+    def __init__(self, browser, autocomplete_light_id):
+        self.id = autocomplete_light_id
+        self.browser = browser
+
+    def component(self):
+        return self.browser.find_by_id(self.id)
+
+    def attr(self, name):
+        return self.browser.evaluate_script(
+            f'document.getElementById("{self.id}").' + name
+        )
+
+    def input(self):
+        return self.attr('input')
+
+    def box(self):
+        return self.attr('box')
+
+    def hilight(self):
+        return self.browser.evaluate_script(
+            f'document.getElementById("{self.id}").box.querySelector("[data-value].hilight")'
+        )
+
+    def choices(self):
+        return self.browser.evaluate_script(
+            f'document.getElementById("{self.id}").box.querySelectorAll("[data-value]")'
+        )
+
+    def focus(self):
+        return self.input().click()
+
+    def defocus(self):
+        return self.browser.find_by_css('body').click()
+
+    def type(self, val):
+        return self.browser.find_by_css(f'#input-simple input')[0].type(val)
+
+
+def get_input(browser, id):
+    return retry(browser.evaluate_script)(
+        f'document.getElementById("{id}").input',
+    )
+
+
+def test_example_is_working(browser):
+    browser.visit('http://localhost:8080')
+    al = AutocompleteLight(browser, 'input-simple')
+
+    # wait until web component has initialized
+    inpt = retry(al.input)
+    assert inpt
+
+    # box should not have been created yet: it's supposed to be lazy
+    assert not al.box()
+
+    # focus on input however, should create the box and display it
+    inpt.click()
+
+    # however we want to retry due to the async nature of the browser ofc
+    box = retry(al.box)
+    assert box
+    assert not box.get_property('hidden')
+
+    # all 4 test choices should be visible
+    assert retry(lambda: len(al.choices()) == 4)
+
+    # de-focus should hide the box
+    al.defocus()
+    assert retry(lambda: al.box().get_property('hidden'))
+
+    # let's try filtering out a choice
+    al.type('a')
+
+    # should show the box again
+    assert retry(lambda: not box.get_property('hidden'))
+
+    # 3 test choices should be visible
+    assert retry(lambda: len(al.choices()) == 3)
+
+    choices = al.choices()
+
+    # let's try some keyboard navigation ...
+    for i in range(3):
+        al.type(Keys.DOWN)
+        assert al.hilight() == choices[i]
+
+    # ... all the way back to the top!
+    al.type(Keys.DOWN)
+    assert al.hilight() == choices[0]
+
+    # and the other way around too ...
+    for i in range(3):
+        al.type(Keys.UP)
+        assert al.hilight() == choices[2 - i]
+
+    # ... back to the bottom!
+    al.type(Keys.UP)
+    assert al.hilight() == choices[2]
+
+    # try enter to select
+    expected = choices[2].get_attribute('data-value')
+    al.type(Keys.ENTER)
+    assert al.input().get_attribute('value') == expected
